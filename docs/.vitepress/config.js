a74e4d260d1f2fdeaaf5a35e2a05049f2e4feb90
AlisaLiCn <alisalicn@gmail.com>
2022-08-22 10:13:39 +0800

    feat: update vitepress config

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,5 +1,36 @@
-module.exports = {
-    base: '/fe-interview/',
-    title: 'Front End Interview',
-    description: 'A collection of Front-End interview questions and study materials'
+import { defineConfig } from 'vitepress'
+
+const nav = [
+  {
+    text: 'Basics',
+    activeMatch: `^/(basics|html|css|javascript)/`,
+    items: [
+      { text: 'HTML', link: '/html/' },
+      { text: 'CSS', link: '/css/' },
+      { text: 'JavaScript', link: '/javascript/' },  
+    ]
+  },
+  {
+    text: 'Framework',
+    activeMatch: `^/framework/`,
+    link: '/framework/'
+  },
+]
+const sidebar = {}
+
+export default defineConfig({
+  base: '/fe-interview/',
+  title: 'Front End Interview',
+  description: 'A collection of Front-End interview questions and study materials',
+  themeConfig: {
+    nav,
+    sidebar,
+    footer: {      
+      license: {
+        text: 'MIT License',
+        link: 'https://opensource.org/licenses/MIT'
+      },
+      copyright: `Copyright © 2022-${new Date().getFullYear()} <a href="https://github.com/alisalicn" target="_blank">Alisa Li</a>`
+    },
   }
+})
